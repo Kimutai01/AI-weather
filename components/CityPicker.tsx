c504b9b47ae64b0f84bcc8d24d42bb307b9e6356
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-04-22 18:35:20 +0300

    Added city functionality

--- components/CityPicker.tsx
+++ components/CityPicker.tsx
@@ -2,12 +2,24 @@
 import React, { useState } from "react";
 import Select from "react-select";
 import { Country, State, City } from "country-state-city";
+import { useRouter } from "next/navigation";
 
 type option = {
   value: {
     latitude: number;
     longitude: number;
     isoCode: string;
+  };
+  label: string;
+} | null;
+
+type Cityoption = {
+  value: {
+    latitude: number;
+    longitude: number;
+    countryCode: string;
+    name: string;
+    stateCode: string;
   };
   label: string;
 } | null;
@@ -21,10 +33,26 @@ const options = Country.getAllCountries().map((country) => ({
 }));
 
 const CityPicker = () => {
-  const [selectedOption, setSelectedOption] = useState<option>();
+  const [selectedCountry, setSelectedCountry] = useState<option>();
+  const [selectedCity, setSelectedCity] = useState<Cityoption>();
+
+  const handleSelectCountry = (option: option) => {
+    setSelectedCountry(option);
+    setSelectedCity(null);
+  };
+
+  const router = useRouter();
   return (
     <div>
-      <Select options={options} />
+      <div>
+        <label htmlFor="country">Country</label>
+      </div>
+      <Select
+        className="text-black"
+        value={selectedCountry}
+        onChange={handleSelectCountry}
+        options={options}
+      />
     </div>
   );
 };
