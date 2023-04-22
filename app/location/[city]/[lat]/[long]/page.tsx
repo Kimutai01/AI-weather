55685b0afb548d57e40a5d1d660344887fcfc156
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-04-23 00:23:00 +0300

    Stats components

--- app/location/[city]/[lat]/[long]/page.tsx
+++ app/location/[city]/[lat]/[long]/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { getClient } from "@/apollo-client";
 import fetchWeatherQueries from "@/graphql/queries/fetchWeatherQueries";
 import CallOutCard from "@/components/CallOutCard";
+import StatCard from "@/components/StatCard";
 
 type Props = {
   params: {
@@ -39,8 +40,54 @@ async function WeatherPage({ params: { city, lat, long } }: Props) {
             {results.timezone})
           </p>
         </div>
-        <div>
+        <div className="m-2 mb-10">
           <CallOutCard message="Hello" />
+        </div>
+
+        <div className="grid grid-cols-1 xl:grid-cols-2 gap-5 m-2">
+          <StatCard
+            title="Maximun Temperature"
+            metric={`${results.daily.temperature_2m_max[0].toFixed(1)}°C`}
+            color="yellow"
+          />
+          <StatCard
+            title="Maximun Temperature"
+            metric={`${results.daily.temperature_2m_min[0].toFixed(1)}°C`}
+            color="green"
+          />
+          <div>
+            <StatCard
+              title="UV Index"
+              metric={`${results.daily.uv_index_max[0].toFixed(1)}°C`}
+              color="yellow"
+            />
+            {Number(results.daily.uv_index_max[0].toFixed(1)) > 5 && (
+              <CallOutCard
+                message="UV Index is high, please wear sunscreen"
+                warning
+              />
+            )}
+          </div>
+
+          <div className="flex space-x-3">
+            <StatCard
+              title="Wind speed"
+              metric={`${results.current_weather.windspeed.toFixed(1)}°C`}
+              color="cyan"
+            />
+            <StatCard
+              title="Wind direction"
+              metric={`${results.current_weather.winddirection.toFixed(1)}°C`}
+              color="violet"
+            />
+          </div>
+        </div>
+        <hr className="mb-5" />
+        <div className="space-y-3">
+          {/* Temp chart */}
+          
+          {/* Rain chart */}
+          {/* Humidity chart */}
         </div>
       </div>
     </div>
