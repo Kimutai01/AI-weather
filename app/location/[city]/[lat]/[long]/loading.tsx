baddb95c57c7e487b1294fbb2db66b49938a5bc0
Kimutai01 <kiprotichkimutai01@gmail.com>
2023-04-23 22:22:27 +0300

    Updated loader

--- app/location/[city]/[lat]/[long]/loading.tsx
+++ app/location/[city]/[lat]/[long]/loading.tsx
@@ -4,7 +4,7 @@ import React from "react";
 function Loading() {
   return (
     <div className="bg-gradient-to-r from-blue-400 to-blue-500 p-10 min-h-screen flex flex-col items-center justify-center text-slate-500">
-      <SunIcon className="h-20 w-20 animate-spin color-[yellow]" />
+      <SunIcon className="h-20 w-20 animate-bounce text-[yellow]" />
       <h2 className="text-4xl font-bold">Loading...</h2>
       <h2 className="text-4xl font-bold">
         Please wait we are calculating the numbers & generating AI summary of
